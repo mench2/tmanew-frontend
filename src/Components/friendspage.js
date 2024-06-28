364d8ea6b3c5b48c3fdfc5c0f8cbbe5d4b534514
mench2 <130401925+mench2@users.noreply.github.com>
2024-06-28 15:49:59 +0300

    fresh4

--- src/Components/friendspage.js
+++ src/Components/friendspage.js
@@ -1,15 +1,18 @@
+import FloatingButton from './FloatingButton';
 import './friends.css'; 
 const tg = window.Telegram.WebApp
 
 const Friends = () => {
-
     const onClose = () => {
         tg.close()
     }
     return (
+        <>
         <div>
             <button onClick={onClose}>Press f</button>
         </div>
+        <FloatingButton onClick={onClose} />
+        </>
     );
 };
 export {Friends};
