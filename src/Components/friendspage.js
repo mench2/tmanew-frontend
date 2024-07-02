48e6ad70f0333cd93f47bdc717b48cb1026f5ace
mench2 <130401925+mench2@users.noreply.github.com>
2024-07-02 19:26:08 +0300

    fres9/33/4

--- src/Components/friendspage.js
+++ src/Components/friendspage.js
@@ -5,11 +5,19 @@ import './friends.css';
 
 const Friends = () => {
     const {user, onClose} = useTelegram();
-
+    const copyTextToClipboard = async (text) => {
+        try {
+          await navigator.clipboard.writeText(text);
+          console.log('Текст успешно скопирован в буфер обмена!');
+        } catch (err) {
+          console.error('Ошибка:', err);
+        }
+      };
+    
     return (
         <>
         <h2>Hello <span className={'username'}>{user?.first_name}</span> 👋 <br />invite friends</h2>
-        <FloatingButton onClick={onClose} />
+        <FloatingButton onClick={() => {onClose(); copyTextToClipboard('ссылка для друзей');}} />
         </>
 
     );
