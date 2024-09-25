57a8a5e0bc95bc504d147a36021b5dd5925b10dd
mench2 <130401925+mench2@users.noreply.github.com>
2024-09-25 19:59:25 +0300

    fres8/894

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { Home } from './Components/homepage';
 import { useSwipeable } from 'react-swipeable';
 import { Ipage } from './Components/ipage';
@@ -10,18 +10,19 @@ import { Friends } from './Components/friendspage';
 
 const App = () => {
   const pages = ['/i', '/', '/friends']; 
-const app = window.Telegram.WebApp;
-app.ready()
-app.isClosingConfirmationEnabled = true;
-app.isVerticalSwipesEnabled = false;
-const navigate = useNavigate();
+  const app = window.Telegram.WebApp;
+  app.ready()
+  app.isClosingConfirmationEnabled = true;
+  app.isVerticalSwipesEnabled = false;
+  
+  const navigate = useNavigate();
 
-const handlers = useSwipeable({
-  onSwipedLeft: () => swipePage(1),
-  onSwipedRight: () => swipePage(-1),
-  preventDefaultTouchmoveEvent: true,
-});
-  const swipePage = (direction) => {
+  const handlers = useSwipeable({
+    onSwipedLeft: () => swipePage(1),
+    onSwipedRight: () => swipePage(-1),
+    preventDefaultTouchmoveEvent: true,
+  });
+    const swipePage = (direction) => {
     const currentPath = window.location.pathname; // Текущий путь
     const currentIndex = pages.indexOf(currentPath);
     const nextIndex = currentIndex + direction;
