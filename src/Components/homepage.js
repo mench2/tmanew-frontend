124607fe909e00a9536626b9c479bde1c5d009c1
mench2 <130401925+mench2@users.noreply.github.com>
2024-10-06 19:11:00 +0300

    fres8/900

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -5,12 +5,8 @@ import RotatingBall from './RotatingBall';
 
 const Home = () => (
   <>
-  
-  <div className='block'>
     <h1>Adeita</h1>
     <RotatingBall />
-  </div>
-  <div className='block2'></div>
 </>
 );
 
